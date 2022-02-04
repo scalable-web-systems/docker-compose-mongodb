cd937119687fe6cbc6619c0697067aa1fb414921
bunny0310 <ikhurana@umass.edu>
2022-02-03 21:41:14 -0500

    wrapped code inside try block, pulled out db connection call from individual endpoints

--- src/comments/index.js
+++ src/comments/index.js
@@ -28,104 +28,88 @@ const connectToDatabase = async () => {
     }
 }
 
-app.get('/', async (req, res) => {
+const runServer = async () => {
     try {
         const connection = await connectToDatabase()
         if (!connection) {
             throw new Error("Unable to connect to database.")
         }
-
-        const collection = connection.collection(commentsCollectionName)
-        let comments = await collection
-            .find({})
-            .toArray()
-
-        return res.status(200).json(comments)
+    
+        app.get('/', async (req, res) => {
+            try {
+        
+                const collection = connection.collection(commentsCollectionName)
+                let comments = await collection
+                    .find({})
+                    .toArray()
+        
+                return res.status(200).json(comments)
+            }
+            catch (error) {
+                console.error(error)
+                return res.status(500).json({"error": error.message})
+            }
+        })
+        
+        app.post('/', async (req, res) => {
+            const payload = req.body
+            const { postId, message } = payload
+            try {
+                if (postId == null || message == null) {
+                    throw new Error("Incorrect payload")
+                }
+                const postServiceName = process.env.POSTS
+                if (!postServiceName) {
+                    return res.status(400).json({"msg": "Environment variable for posts service name not set!"})
+                }
+                const fetchPostRequest = await axios.get(`http://${postServiceName}:${port}/${postId}`)
+                const post = await fetchPostRequest.data
+                if (!post) {
+                    return res.status(400).json({"msg": `Post with ID #${postId} not found!`})
+                }
+        
+                const connection = await connectToDatabase()
+                if (!connection) {
+                    throw new Error("Unable to connect to database.")
+                }
+        
+                const collection = connection.collection(commentsCollectionName)
+                const comment = await collection.insertOne(payload)
+                return res.status(201).json(comment)
+            }
+            catch (error) {
+                console.error(error)
+                return res.status(500).json({error: error.message})
+            }
+        })
+        
+        app.get('/:id', async (req, res) => {
+            try {
+                const postId = req.params['id']
+                console.log(`Incoming request to return comments associated with post ID #${postId}`)
+                const connection = await connectToDatabase()
+                if (!connection) {
+                    throw new Error("Unable to connect to database.")
+                }
+        
+                const collection = connection.collection(commentsCollectionName)
+                const comments = await collection.find({postId: postId}).toArray()
+                return res.status(200).json(comments)
+            }
+            catch(error) {
+                console.error(error)
+                return res.status(500).json({"error": error.message})
+            }
+        })
+    
+        app.listen(port, '0.0.0.0', () => {
+            console.log(`Server listening on port ${port}`)
+        })
+    
     }
     catch (error) {
-        console.error(error)
-        return res.status(500).json({"error": error.message})
+        console.error(error.message)
     }
-})
+}
 
-app.post('/', async (req, res) => {
-    const payload = req.body
-    const { postId, message } = payload
-    try {
-        if (postId == null || message == null) {
-            throw new Error("Incorrect payload")
-        }
-        const postServiceName = process.env.POSTS
-        if (!postServiceName) {
-            return res.status(400).json({"msg": "Environment variable for posts service name not set!"})
-        }
-        const fetchPostRequest = await axios.get(`http://${postServiceName}:${port}/${postId}`)
-        const post = await fetchPostRequest.data
-        if (!post) {
-            return res.status(400).json({"msg": `Post with ID #${postId} not found!`})
-        }
-
-        const connection = await connectToDatabase()
-        if (!connection) {
-            throw new Error("Unable to connect to database.")
-        }
-
-        const collection = connection.collection(commentsCollectionName)
-        const comment = await collection.insertOne(payload)
-        return res.status(201).json(comment)
-    }
-    catch (error) {
-        console.error(error)
-        return res.status(500).json({error: error.message})
-    }
-})
-
-app.get('/:id', async (req, res) => {
-    try {
-        const postId = req.params['id']
-        console.log(`Incoming request to return comments associated with post ID #${postId}`)
-        const connection = await connectToDatabase()
-        if (!connection) {
-            throw new Error("Unable to connect to database.")
-        }
-
-        const collection = connection.collection(commentsCollectionName)
-        const comments = await collection.find({postId: postId}).toArray()
-        return res.status(200).json(comments)
-    }
-    catch(error) {
-        console.error(error)
-        return res.status(500).json({"error": error.message})
-    }
-})
-
-/*
-    * OVER ENGINEERED HERE, DELETE ENDPOINT NOT NECESSARY FOR THE GATEWAY TUTORIAL
-*/
-// app.delete('/:postId', (req, res) => {
-//     try {
-//         const postId = parseInt(req.params['postId'])
-//         console.log(`Incoming request to delete comments associated with post ID # ${postId}`)
-//         const filteredComments = comments
-//             .filter(c => c.postId === postId)
-//         filteredComments
-//             .forEach(
-//                 fc => comments.splice(
-//                     comments.indexOf(comments.find(c => c.id === fc.id)),
-//                     1,
-//                 )
-//             )
-//         return res.status(200).json({
-//             "msg": `All comments associated with the post #${postId} have been deleted`, 
-//             "data": comments
-//         })
-//     }
-//     catch(error) {
-//         console.error(error)
-//         return res.status(500).json({error: error.message})
-//     }
-// })
-
-app.listen(port, '0.0.0.0', () => {
-    console.log(`Server listening on port ${port}`)
-})
+runServer()
--- src/posts/index.js
+++ src/posts/index.js
@@ -29,115 +29,89 @@ const connectToDatabase = async () => {
     }
 }
 
-app.get('/', async (req, res) => {
+const runServer = async () => {
     try {
+
         const connection = await connectToDatabase()
         if (!connection) {
             throw new Error("Unable to connect to database.")
         }
-
-        const commentServiceName = process.env.COMMENTS
-        if (!commentServiceName) {
-            return res.status(400).json({"error": "Comments service name environment variable - COMMENTS not defined."})
-        }
-
-        const collection = connection.collection(postsCollection)
-        let posts = await collection
-            .find({})
-            .toArray()
-        
-        posts = await Promise.all(posts
-            .map(async (post) => {
-                const comments = await (await axios.get(`http://${commentServiceName}:${port}/${post._id.toHexString()}`)).data
-                return {
-                    ...post,
-                    comments
+    
+        app.get('/', async (req, res) => {
+            try {
+                const commentServiceName = process.env.COMMENTS
+                if (!commentServiceName) {
+                    return res.status(400).json({"error": "Comments service name environment variable - COMMENTS not defined."})
                 }
-            }))
-
-        return res.status(200).json(posts)
-    }
-    catch (error) {
-        console.error(error)
-        return res.status(500).json({"error": error.message})
-    }
-})
-
-app.get('/:id', async (req, res) => {
-    try {
-        const _id = req.params['id']
-        const oid = new ObjectId(_id)
-        console.log(`Incoming request to find post with ID #${_id}`)
-
-        const connection = await connectToDatabase()
-        if (!connection) {
-            throw new Error("Unable to connect to database.")
-        }
-
-        const collection = connection.collection(postsCollection)
-        let post = await collection
-            .findOne({_id: oid})
         
-        console.log(post)
-        return res.status(post ? 200 : 404).json(post)
+                const collection = connection.collection(postsCollection)
+                let posts = await collection
+                    .find({})
+                    .toArray()
+                
+                posts = await Promise.all(posts
+                    .map(async (post) => {
+                        const comments = await (await axios.get(`http://${commentServiceName}:${port}/${post._id.toHexString()}`)).data
+                        return {
+                            ...post,
+                            comments
+                        }
+                    }))
+        
+                return res.status(200).json(posts)
+            }
+            catch (error) {
+                console.error(error)
+                return res.status(500).json({"error": error.message})
+            }
+        })
+        
+        app.get('/:id', async (req, res) => {
+            try {
+                const _id = req.params['id']
+                const oid = new ObjectId(_id)
+                console.log(`Incoming request to find post with ID #${_id}`)
+
+                const collection = connection.collection(postsCollection)
+                let post = await collection
+                    .findOne({_id: oid})
+                
+                console.log(post)
+                return res.status(post ? 200 : 404).json(post)
+            }
+            catch (error) {
+                return res.status(500).json({"error": error.message})
+            }
+        })
+        
+        app.post('/', async (req, res) => {
+            const payload = req.body
+            const { title, description } = payload
+            try {
+                if (title == null || description == null) {
+                    throw new Error("Incorrect payload")
+                }
+        
+                const collection = connection.collection(postsCollection)
+                const post = await collection
+                    .insertOne(payload)
+        
+                return res.status(201).json(post)
+            }
+            catch (error) {
+                console.error(error)
+                return res.status(500).json({error: error.message})
+            }
+        })
+    
+        app.listen(port, '0.0.0.0', () => {
+            console.log(`Server listening on port ${port}`)
+        })
     }
+    
     catch (error) {
-        return res.status(500).json({"error": error.message})
+        console.error(error.message)
     }
-})
+}
 
-app.post('/', async (req, res) => {
-    const payload = req.body
-    const { title, description } = payload
-    try {
-        if (title == null || description == null) {
-            throw new Error("Incorrect payload")
-        }
-
-        const connection = await connectToDatabase()
-        if (!connection) {
-            throw new Error("Unable to connect to database.")
-        }
-
-        const collection = connection.collection(postsCollection)
-        const post = await collection
-            .insertOne(payload)
-
-        return res.status(201).json(post)
-    }
-    catch (error) {
-        console.error(error)
-        return res.status(500).json({error: error.message})
-    }
-})
-
-/*
-    * OVER ENGINEERED HERE, DELETE ENDPOINT NOT NECESSARY FOR THE GATEWAY TUTORIAL
-*/
-// app.delete('/:id', async (req, res) => {
-//     try {
-//         const id = parseInt(req.params['id'])
-//         const post = posts.find(p => p.id === id)
-//         if (!post) {
-//             return res.status(404).json({"error": `Post with ID #${id} not found.`})
-//         }
-//         const commentServiceName = process.env.COMMENTS
-//         if (!commentServiceName) {
-//             return res.status(400).json({"error": "Comments service name environment variable - COMMENTS not defined."})
-//         }
-//         await axios.delete(`http://${commentServiceName}:${port}/${id}`)
-//         posts
-//             .splice(posts.indexOf(post), 1)
-//         return res.status(200).json({
-//             msg: `Post with ID #${id} successfully deleted`,
-//             data: posts,
-//         })
-//     }
-//     catch(error) {
-//         return res.status(500).json({"error": error.message})
-//     }
-// })
-
-app.listen(port, '0.0.0.0', () => {
-    console.log(`Server listening on port ${port}`)
-})
+runServer()
